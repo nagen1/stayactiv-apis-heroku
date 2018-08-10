9f911041cfc31ba48012514bbc9b06ff2a07fad7
nagen-mac <rao.jogi@gmail.com>
2018-08-09 23:48:48 -0500

    next commit

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from taskJson import workoutJson
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Yo, its working!'
+	return 'Hello, its working!'
+
+@app.route('/workouts', methods=['GET'])
+def workouts():
+    try:
+        list = workoutJson
+    except:
+        NoResultFound
+
+    return jsonify({'workouts': list})
+
 
 if __name__ == "__main__":
 	app.run()
