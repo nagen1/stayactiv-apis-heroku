c4ea42c27c951c8f6bfa8789a35313c47b14afd0
nagen-mac <rao.jogi@gmail.com>
2018-08-15 00:59:32 -0500

    import fix 4

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ sys.path.append('/app/app.py')
 from flask import Flask, jsonify
 from taskJson import workoutJson
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
-from app.database.database import Acitvity
+import app.database
+from app.database import Acitvity
 from sqlalchemy import create_engine, and_, distinct
 from sqlalchemy.orm import sessionmaker
 
